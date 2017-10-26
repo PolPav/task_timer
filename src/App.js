f1231e301546fe6d6195ee5f7206e73ef8f0ba89
Oleg <krtlmail@gmail.com>
2017-10-26 14:26:22 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,72 +1,13 @@
 import React, { Component } from 'react';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 import Timer from './components/Timer';
-import allReducers from './reducers';
-
-let store = createStore(allReducers);
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {past_time: 0, init: 0};
-  }
-
-  toggleTimer = () => {
-
-    let button = document.getElementById('toggleButton');
-
-    if(button.innerHTML === 'Start'){
-        this.timer = setInterval(
-          () => this.differenceTime(),
-          1000
-        );
-      button.innerHTML = 'Stop';
-
-    } else {
-        clearInterval(this.timer);
-        button.innerHTML = 'Start';
-    }
-  };
-
-  differenceTime = () => {
-    this.setState({past_time: new Date() - this.props.start});
-  };
-
   render() {
-    let past_time = Math.round(this.state.past_time / 1000);
-
-    let seconds = past_time % 60;
-        past_time = past_time - seconds;
-        past_time = Math.round(past_time / 60);
-    let minutes = past_time % 60;
-        past_time = past_time - minutes;
-        past_time = Math.round(past_time / 60);
-    let hours = past_time % 60;
-
-        if(seconds < 10){
-           seconds = "0"+seconds;
-        }
-
-        if(minutes < 10){
-           minutes = "0"+minutes;
-        }
-
-        if(hours < 10){
-           hours = "0"+hours;
-        }
 
     return (
       <div className={'wrap_timer'}>
-        <input className={'wrap_timer_input'} value={hours+":"+minutes+":"+seconds}/>
-        <div className={'wrap_timer_buttons'}>
-          <button id={"toggleButton"} className={'wrap_timer_button'} onClick={this.toggleTimer}>Start</button>
-        </div>
-
-        {/*<Provider store={store}>*/}
-              {/*<Timer/>*/}
-        {/*</Provider>*/}
+        <Timer/>
       </div>
     );
   }
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,14 +1,81 @@
 import React, { Component } from 'react';
-import TaskList from '../containers/TaskList';
 
-export default class Timer extends Component{
-  render(){
+class Timer extends Component {
+
+  constructor(props) {
+
+    super(props);
+    this.state = {past_time: 0, init:0, value: '', timeDefault: 0};
+  }
+
+  toggleTimer = () => {
+
+    this.nowDate = Date.now();
+    let button = document.querySelector('button#toggleButton');
+    let timeResult = document.querySelector('h3#testText');
+
+      if(button.innerHTML === 'Start'){
+        this.timer = setInterval(
+          () => this.differenceTime(),
+          1000
+        );
+
+        button.innerHTML = 'Stop';
+
+      } else {
+
+        clearInterval(this.timer);
+        button.innerHTML = 'Start';
+        this.setState({value: timeResult.innerHTML});
+      }
+  };
+
+  differenceTime = () => {
+
+    this.setState({past_time: new Date() - this.nowDate});
+  };
+
+  render() {
+
+    let past_time = Math.round(this.state.past_time / 1000);
+    let seconds = past_time % 60;
+        past_time = past_time - seconds;
+        past_time = Math.round(past_time / 60);
+
+    let minutes = past_time % 60;
+        past_time = past_time - minutes;
+        past_time = Math.round(past_time / 60);
+
+    let hours = past_time % 60;
+
+      if(seconds < 10){
+        seconds = "0"+seconds;
+      }
+
+      if(minutes < 10){
+        minutes = "0"+minutes;
+      }
+
+      if(hours < 10){
+        hours = "0"+hours;
+      }
+
     return (
-      <div>
-        <TaskList/>
+      <div className={'wrap_timer'}>
+        <div className={'wrap_timer_stopped'}>
+          <h3 id={'testText'}>{hours+":"+minutes+":"+seconds}</h3>
+        </div>
+        <div className={'wrap_timer_buttons'}>
+          <button id={'toggleButton'} className={'wrap_timer_button'} onClick={this.toggleTimer}>Start</button>
+        </div>
+        <div>
+          <h3 id={'stoppedTime'}>{this.state.value}</h3>
+        </div>
       </div>
-    )
+    );
   }
 }
+
+export default Timer;
 
 
